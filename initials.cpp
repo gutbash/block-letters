51c1934dcaa51b04093baf26c31abdcc0d67ac18
Sebastian <44552816+bazeroni@users.noreply.github.com>
2021-04-17 20:08:48 -0400

    Create initials.cpp

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// Sebastian likes to code.
+
+#include <iostream>
+
+int main() {
+
+  std::cout << " SSS   GGG \n";
+  std::cout << "S   S G   G\n";
+  std::cout << "S     G   \n";
+  std::cout << " SSS  GGGGG\n";
+  std::cout << "    S G   G\n";
+  std::cout << "S   S G   G\n";
+  std::cout << " SSS   GGG \n";
+
+}
